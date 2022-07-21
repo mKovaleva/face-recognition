8e1386be4f25a0a411003b5b580b720a6dbe767f
Maria Kovaleva <73141603+mKovaleva@users.noreply.github.com>
2022-07-21 22:10:34 +0300

    Update face_rec_mod.py

--- face_rec_mod.py
+++ face_rec_mod.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
+#   You can get the trained model file from:
+#   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2.
 
 import dlib
 
@@ -9,8 +7,6 @@ sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
 detector = dlib.get_frontal_face_detector()
 
-
-# In[ ]:
 
 
 
